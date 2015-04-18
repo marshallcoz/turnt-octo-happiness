8bf2205884018cbe35c7376066da4e23a4c31e96
Marcial Contreras <marcialcz@gmail.com>
2015-04-18 10:43:09 -0500

    again

--- LayIBEM3d/sismogramaA.m
+++ LayIBEM3d/sismogramaA.m
@@ -1,7 +1,7 @@
 % La mitad de arriba del sismograma, con la frecuencia
 function sismogramaA(iRecep,f_vars,s,comp,dirFza,center)
 name = ['G' num2str(comp) num2str(dirFza) '_(' num2str(iRecep) ')'];
-figure('Name',name);hold on; set(gcf,'Visible', 'off');
+figure('Name',name);hold on%; set(gcf,'Visible', 'off');
 subplot(2,1,1)
 plot((0:f_vars.ntiempo-1).*f_vars.DFREC,real(s(comp,:)),'r');hold on;
 plot((0:f_vars.ntiempo-1).*f_vars.DFREC,imag(s(comp,:)),'b');
