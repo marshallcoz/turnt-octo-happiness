7675f499a2841192b72c51542ef0d27b33e1562e
Marcial Contreras <marcialcz@gmail.com>
2015-06-21 20:24:34 -0500

    copy

--- DWLIBEM/Workdir/copiar.sh
+++ DWLIBEM/Workdir/copiar.sh
@@ -17,10 +17,11 @@ for ((i=1; i<=7; i++)); do
     echo "copiando carpeta" $i
     cp traces$i/W$i.bin $DESTINO/traces$i
     cp video$i/G$i.bin $DESTINO/video$i
-    
+    cp video$i/0_MecElemvideo.mp4 $DESTINO/video$i
+    cp video$i/0_video.mp4 $DESTINO/video$i
 done
 
-cp insbackup $DESTINO/ins
+cp -r insbackup $DESTINO/ins
 cp logfile.txt $DESTINO
 
 echo listo
